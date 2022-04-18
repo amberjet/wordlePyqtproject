1563d453c3d350905ecb2774edb8188ef91af8df
amberjet <kuroamber@gmail.com>
2022-04-18 23:18:50 +0300

    Рабочий прототип: отмечает буквы на угаданных позициях. Пока без визуальной части

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ import random
 def main():
     with open('files/words.txt') as f:
         lines = f.readlines()
-    today_word = random.choice(lines)
+    today_word = random.choice(lines).strip()
     print(today_word)
-    check = ''
-    while check != today_word:
+    while True:
         guess = input('Введите слово из 5 букв: ')
         if len(guess) < 5:
             continue
-        check = suspicion(today_word, guess)
-        if check == today_word:
+        if guess == today_word:
             print('вы угадали!')
             break
         else:
-            print(check)
+            print(suspicion(today_word, guess))
 
 
 def suspicion(today_word, guess):
