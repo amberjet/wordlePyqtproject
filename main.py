080f21fc391a55fd00292ec16b4e79ac0a615709
amberjet <kuroamber@gmail.com>
2022-04-21 14:18:33 +0300

    Добавлен ui-файл

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import random
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('files/wordle.ui', self)
+        self.run()
+
+    def run(self):
+        pass
 
 
 def main():
@@ -6,9 +19,9 @@ def main():
         lines = [x.strip() for x in f.readlines()]
     today_word = random.choice(lines).strip()
     print(today_word)
-    while True:
+    for _ in range(5):
         guess = input('Введите слово из 5 букв: ')
-        if len(guess) < 5 or guess not in lines:
+        if len(guess) != 5 or guess not in lines:
             continue
         if guess == today_word:
             print('вы угадали!')
@@ -35,8 +48,13 @@ def suspicion(today_word, guess):
             true_symbol.remove(symb)
         if today_word.count(symb) < true_symbol.count(symb):
             true_symbol.remove(symb)
+        if today_word.count(symb) < res.count(symb) + true_symbol.count(symb):
+            true_symbol.remove(symb)
     return res, true_symbol
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
